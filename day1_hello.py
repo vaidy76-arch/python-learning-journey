f25268a343b94ef5c489affd95df63066e81ad66
Vaidyanathan Srinivasan <vaidy76@gmail.com>
2025-11-28 15:00:00 -0600

    Day 1: Bonus challenge

--- day1_hello.py
+++ day1_hello.py
@@ -3,4 +3,5 @@ print("Hello, World!")
 print("Starting my full stack development journey!")
 
 name = input("What's your name? ")
-print(f"Welcome to Python, {name}! Let's learn together.")
+year = input("your Born year")
+print(f"Welcome to Python, {name} born in {year}! Let's learn Together. ")
